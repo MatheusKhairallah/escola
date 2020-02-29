80278deb6dff7783af597a38fa1fd61f7d72d2e9
MatheusKhairallah <playermaster10like@gmail.com>
2020-02-28 21:23:31 -0300

    Adicionamento de programa c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace escola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
